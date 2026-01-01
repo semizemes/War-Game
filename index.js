921a11da13b2a5e04639dd19964b9e07c007d1ff
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-01 22:13:37 +0500

    feat: add cards draw functionality and its style

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => console.log(data))
-}
+let deckId;
+const cardsContainer = document.getElementById("cards")
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      deckId = data.deck_id;
+    });
+}
+handleClick();
+
+document.getElementById("new-deck").addEventListener("click", handleClick);
+document.getElementById("draw-cards").addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards[0].image);
+      cardsContainer.children[0].innerHTML = `<img src="${data.cards[0].image}">`
+      cardsContainer.children[1].innerHTML = `<img src="${data.cards[1].image}">`
+    });
+});
