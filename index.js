9d4fd1cf496edafcd83e28680930c2a00445f8b2
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-02 12:35:48 +0500

    feat: refactor handleClick to use async/await for improved readability

--- index.js
+++ index.js
@@ -6,45 +6,45 @@ const newDeck = document.getElementById("new-deck");
 let computerScore = 0;
 let myScore = 0;
 
-function handleClick() {
-  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-    .then((res) => res.json())
-    .then((data) => {
-      deckId = data.deck_id;
-      remainingText.innerHTML = `Remaining Cards: ${data.remaining}`;
-    });
+async function handleClick() {
+  const res = await fetch(
+    "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
+  );
+  const data = await res.json();
+  deckId = data.deck_id;
+  remainingText.innerHTML = `Remaining Cards: ${data.remaining}`;
 }
 handleClick();
 
 newDeck.addEventListener("click", handleClick);
-document.getElementById("draw-cards").addEventListener("click", (e) => {
+document.getElementById("draw-cards").addEventListener("click", async (e) => {
   const currentEl = e.currentTarget;
-  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-    .then((res) => res.json())
-    .then((data) => {
-      remainingText.textContent = `Remaining Cards: ${data.remaining}`;
-      cardsContainer.children[0].innerHTML = `<img src="${data.cards[0].image}">`;
-      cardsContainer.children[1].innerHTML = `<img src="${data.cards[1].image}">`;
-      const winnerText = determineCardWinner(
-        data.cards[0].value,
-        data.cards[1].value
-      );
-      score.innerHTML = winnerText;
-      if (data.remaining == 0) {
-        currentEl.disabled = true;
+  const res = await fetch(
+    `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
+  );
+  const data = await res.json();
+  remainingText.textContent = `Remaining Cards: ${data.remaining}`;
+  cardsContainer.children[0].innerHTML = `<img src="${data.cards[0].image}">`;
+  cardsContainer.children[1].innerHTML = `<img src="${data.cards[1].image}">`;
+  const winnerText = determineCardWinner(
+    data.cards[0].value,
+    data.cards[1].value
+  );
+  score.innerHTML = winnerText;
+  if (data.remaining == 0) {
+    currentEl.disabled = true;
 
-        if (computerScore > myScore) {
-          score.textContent = "The computer won the game!";
-        } else if (computerScore < myScore) {
-          score.textContent = "You won the game!";
-        } else {
-          score.textContent = "It's a tie game!";
-        }
-        newDeck.addEventListener("click", () => {
-          window.location.reload();
-        });
-      }
+    if (computerScore > myScore) {
+      score.textContent = "The computer won the game!";
+    } else if (computerScore < myScore) {
+      score.textContent = "You won the game!";
+    } else {
+      score.textContent = "It's a tie game!";
+    }
+    newDeck.addEventListener("click", () => {
+      window.location.reload();
     });
+  }
 });
 
 function determineCardWinner(card1, card2) {
